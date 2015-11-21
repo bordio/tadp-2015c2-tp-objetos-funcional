03731c935ef1d58f825649b35f8d3ee84d68afd1
Federico Cano <canofedericomartin@gmail.com>
2015-11-21 19:46:36 -0300

    Creo seed de tests

--- src/test/scala/ar/edu/tadp/dragonball/GuerreroSpec.scala
+++ src/test/scala/ar/edu/tadp/dragonball/GuerreroSpec.scala
@@ -4,25 +4,10 @@ import ar.edu.tadp.dragonball.Criterios._
 import ar.edu.tadp.dragonball.Movimientos._
 import ar.edu.tadp.dragonball.TiposDeDigestion._
 import org.scalatest.{ShouldMatchers, FunSpec}
+import ar.edu.tadp.dragonball.seed._
 import scala.util.Try
 
 class GuerreroSpec extends FunSpec with ShouldMatchers {
-
-  val Ataques: List[Movimiento] = List(DejarseFajar, CargarKi, MuchosGolpesNinja, Onda(150), Genkidama)
-  def tieneMasEnergia(guerreros: Guerreros) = guerreros._1.energia
-  val AtaquesDeYajirobe: List[Movimiento] = List(MuchosGolpesNinja, UsarItem(ArmaFilosa), UsarItem(SemillaDelErmitanio))
-  val hacertePensar: Function1[Guerreros,Guerreros] = { case (atacante,oponente) => (atacante, oponente.actualizarEnergia(2)) }
-  val goku: Guerrero = Guerrero("goku", List(FotoDeLaLuna, EsferaDelDragon(4)), 100, 1000, Saiyajin(Normal,false), Luchando, Ataques)
-  val vegeta: Guerrero = Guerrero("vegeta", List(FotoDeLaLuna,SemillaDelErmitanio), 500, 1000, Saiyajin(Normal), Luchando, Ataques ++ List(UsarItem(FotoDeLaLuna)))
-  val trunks: Guerrero = Guerrero("trunks", List(SemillaDelErmitanio, ArmaFilosa), 1350, 2000, Saiyajin(SuperSaiyajin(1), cola = false), Luchando, Ataques)
-  val androide18: Guerrero = Guerrero("Androide18", List(ArmaDeFuego, Municion(ArmaDeFuego)), 900, 1800, Androide, Luchando, Ataques ++ List(Explotar,UsarItem(FotoDeLaLuna)))
-  val androide17: Guerrero = Guerrero("Androide17", List(ArmaDeFuego, Municion(ArmaDeFuego)), 1800, 5000, Androide, Luchando, Ataques ++ List(Explotar,UsarItem(FotoDeLaLuna)))
-  val yajirobe: Guerrero = Guerrero("Yajirobe", List(SemillaDelErmitanio), 400, 400, Humano, Luchando, AtaquesDeYajirobe)
-  val androideDebil: Guerrero = Guerrero("Androide16", List(ArmaDeFuego, Municion(ArmaDeFuego)), 200, 300, Androide, Luchando, Ataques ++ List(Explotar, Onda(80)))
-  val cell: Guerrero = Guerrero("Cell", List(EsferaDelDragon(3)), 1200, 3000, Monstruo(digestionCell,List()), Luchando, Ataques ++ List(ComerseAlOponente))
-  val majinBoo: Guerrero = Guerrero("MajinBoo", List(), 2000, 3000, Monstruo(digestionMajinBoo, List()), Luchando, Ataques ++ List(ComerseAlOponente))
-  val piccolo: Guerrero = Guerrero("Piccolo", List(), 1000, 1000, Namekusein, Luchando, Ataques ++ List(Magia(hacertePensar)))
-  val yamcha: Guerrero = Guerrero("Yamcha",List(EsferaDelDragon(1),EsferaDelDragon(2),EsferaDelDragon(3),EsferaDelDragon(4),EsferaDelDragon(5),EsferaDelDragon(6),EsferaDelDragon(7)),50,200,Humano,Luchando,Ataques)
 
   describe ("Constructor") {
     it ("Goku should have") {
